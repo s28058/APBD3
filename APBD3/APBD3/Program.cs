eb43176104dccf61eb331f2dc42e1a71c7c35295
s28058 <s28058@pjwstk.edu.pl>
2024-03-24 18:02:03 +0100

    Test notifications

--- APBD3/APBD3/Program.cs
+++ APBD3/APBD3/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using APBD3.Models;
 
-Console.WriteLine("Hello, World!");
+var containerNumber = new SerialNumber(ContainerType.Liquid, 1);
+var container = new LiquidContainer(100, 20, 10, containerNumber, 100, CargoDanger.Hazardous);
+
+container.Subscribe(HandleNotification);
+
+container.LoadCargo(80);
+
+Console.WriteLine("I'm done :D");
+
+void HandleNotification(HazardNotification notification)
+{
+    var (message, containerNumber) = notification;
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("HAZARD:");
+    Console.ResetColor();
+    Console.WriteLine(message);
+    Console.WriteLine($"In container: {containerNumber}\n");
+}
