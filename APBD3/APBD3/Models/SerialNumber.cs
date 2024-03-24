9c0fb30c4419b4b0e5964b4cfd3aa5201817ac86
s28058 <s28058@pjwstk.edu.pl>
2024-03-24 23:16:41 +0100

    Serial numer IEquatable

--- APBD3/APBD3/Models/SerialNumber.cs
+++ APBD3/APBD3/Models/SerialNumber.cs
@@ -1,6 +1,6 @@
 namespace APBD3.Models;
 
-public class SerialNumber
+public class SerialNumber : IEquatable<SerialNumber>
 {
     private const string Prefix = "KON";
     public ContainerType Type { get; }
@@ -24,6 +24,36 @@ public class SerialNumber
     public override string ToString()
     {
         return $"{Prefix}-{TypeLabel}-{_id}";
+    }
+
+    public bool Equals(SerialNumber? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return _id == other._id && Type == other.Type;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((SerialNumber)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_id, (int)Type);
+    }
+
+    public static bool operator ==(SerialNumber? left, SerialNumber? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(SerialNumber? left, SerialNumber? right)
+    {
+        return !Equals(left, right);
     }
 }
 
